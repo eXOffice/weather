cd54ea7ca184591a41a9b8dc5f53f0ce21b05cbf
eXOffice <sumontobonti@gmail.com>
2023-03-19 20:08:42 +0600

    used 'async' and 'await' for asynchronization wrapped with try catch block.. call latitude and longitude values from another class called custom location.dart file

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,12 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;;
+
 import 'package:weather/services/location.dart';
+
+var url =
+    'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=6ae5b8e29cf3d249e85a4d02e9a037c1';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({Key? key}) : super(key: key);
@@ -30,8 +37,24 @@ class _LoadingScreenState extends State<LoadingScreen> {
     // print(Location().latitude);
   }
 
+  Future<void> getData() async {
+    http.Response response = await http.get(Uri.parse(url));
+    //print(response.body);
+    if (response.statusCode == 200) {
+      String decodeData = response.body;
+      var weatherDescription =
+          jsonDecode(decodeData)['weather'][0]['description'];
+      print(weatherDescription);
+
+      print(decodeData);
+    } else {
+      print(response.statusCode);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    //getData();
     return Scaffold(
       body: SafeArea(
         child: Center(
@@ -41,6 +64,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
               ElevatedButton(
                 onPressed: () {
                   //getLocation();
+                  getData();
                   print(
                       'here is longitude: $longitude and here is latitude: $latitude');
                 },
---
+++ lib/services/networking.dart
@@ -0,0 +1,6 @@
+class NetworkHelper {
+  NetworkHelper(this.url);
+  final String url;
+
+  void getData() async {}
+}
